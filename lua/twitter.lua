d6ef62ce2feee8b59bb74df2953ba19f558409c9
Mark Hutcheson <meh2481@vt.edu>
2015-09-09 20:02:27 -0400

    Accept @mentions also

--- lua/twitter.lua
+++ lua/twitter.lua
@@ -5,11 +5,79 @@ local function tablelength(T)
   return count
 end
 
+local function tweetsat()
+	local channel = getchannel()
+	print("checking twitter mentions...")
+	local urlToGet = "https://api.twitter.com/1.1/statuses/mentions_timeline.json"
+	local numtweets = 5
+	local urlParams1 = "count="..numtweets
+	--local urlParams2 = "screen_name="..tweeter
+	
+	local headerStr = "Authorization: OAuth "
+	local authTab = {
+	["oauth_consumer_key"] = G_OAUTH["consumerkey"],
+	["oauth_nonce"] = encode64(tostring(os.time()*os.time()*3)):gsub("%W",""),	--Has to be different than the other nonce...
+	--["oauth_signature"] = "",
+	["oauth_signature_method"] = "HMAC-SHA1",
+	["oauth_timestamp"] = os.time(),
+	["oauth_token"] = G_OAUTH["accesstoken"],
+	["oauth_version"] = "1.0",
+	}
+	
+	local sigBase = "GET&"
+	sigBase = sigBase..encodeURI(urlToGet)..'&'..encodeURI(urlParams1.."&")
+	
+	--Sort the list
+	local orderedList = {}
+	for k,v in pairs(authTab) do
+		table.insert(orderedList,k)
+	end
+	table.sort(orderedList)
+	for i,n in ipairs(orderedList) do 
+		sigBase = sigBase..encodeURI(n..'='..authTab[n]..'&')
+	end
+	
+	--Strip the last ampersand
+	sigBase = string.sub(sigBase, 0, -4)
+	
+	--sigBase = sigBase..encodeURI(urlParams2)
+	
+	local signKey = G_OAUTH["consumersecret"]..'&'..G_OAUTH["accesstokensecret"]
+	
+	authTab["oauth_signature"] = encodeURI(encode64(hmacSHA1(signKey, sigBase)))	--Hash it
+	
+	headerStr = headerStr.."oauth_consumer_key=\""..authTab["oauth_consumer_key"].."\", oauth_nonce=\""..authTab["oauth_nonce"].."\", oauth_signature=\""..authTab["oauth_signature"].."\", oauth_signature_method=\""..authTab["oauth_signature_method"].."\", oauth_timestamp=\""..authTab["oauth_timestamp"].."\", oauth_token=\""..authTab["oauth_token"].."\", oauth_version=\""..authTab["oauth_version"].."\""
+	
+	--Fetch result from twitter
+	local JSONScript = wget(urlToGet..'?'..urlParams1, headerStr)
+	
+	--print(JSONScript)
+	
+	--Decode into Lua-parsable table
+	local twitter_table = G_JSON:decode(JSONScript)
+	
+	--savetable(twitter_table, "test.txt")
+	
+	--Format and output
+	if twitter_table ~= nil then
+		for i = tablelength(twitter_table), 1, -1 do	--Tweets are posted in reverse order from IRC, so spin backwards over this list to post oldest first
+		    --print("str: "..twitter_table[i]["id_str"].."table: "..G_TWEETS[twitter_table[i]["id_str"]])
+			if G_TWEETS[twitter_table[i]["id_str"]] == nil then	--Haven't posted this tweet yet
+				G_TWEETS[twitter_table[i]["id_str"]] = 1
+				local tweettext = twitter_table[i]["text"]
+				say(channel, "[@"..twitter_table[i]["user"]["screen_name"].."] "..tweettext)
+			end
+		end
+	end
+end
+setglobal("tweetsat", tweetsat)
+
 local function checktwitter(nopost)
+	tweetsat()
 	local channel = getchannel()
 	print("checking the twitterverse...")
 	local urlToGet = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-	local numtweets = 4
+	local numtweets = 5
 	local tweeter = "infinite_ammo"	--TODO: Select different peoples
 	local urlParams1 = "count="..numtweets--.."&"
 	--local urlParams2 = "screen_name="..tweeter
